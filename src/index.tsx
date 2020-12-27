38d35200887abcc7ba75c91841915723db2bd936
Tyler Long <tyler4long@gmail.com>
2020-12-27 12:52:54 -0800

    Add React

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDOM.render(<h1>Hello world</h1>, container);
