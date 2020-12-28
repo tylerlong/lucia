6c867b8679ef897ca1e4739ef8fb575bba9efcca
Tyler Long <tyler4long@gmail.com>
2020-12-27 21:15:09 -0800

    React Ref

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { Slider } from 'antd';
 
 class App extends React.Component {
   sliderRef: React.RefObject<typeof Slider>;
-  imageElements: HTMLInputElement[];
+  inputElements: HTMLInputElement[];
+  imageElements: HTMLImageElement[];
 
   constructor(props: any) {
     super(props);
     this.sliderRef = React.createRef();
+    this.inputElements = [];
     this.imageElements = [];
   }
 
@@ -18,24 +20,25 @@ class App extends React.Component {
       <p>
         Lucia is derived from lux, the Latin word for light.
       </p>
-      <input type="file" id="upload0" onChange={e => this.change(0)} ref={element => this.imageElements.push(element!)}/>
-      <input type="file" id="upload1" onChange={e => this.change(1)} ref={element => this.imageElements.push(element!)}/>
+      <input type="file" id="upload0" onChange={e => this.change(0)} ref={element => this.inputElements.push(element!)}/>
+      <input type="file" id="upload1" onChange={e => this.change(1)} ref={element => this.inputElements.push(element!)}/>
       <br/><br/>Animation speed: <Slider min={1} max={100} defaultValue={10} onAfterChange={() => this.makeGif()} ref={this.sliderRef}/>
       <hr/>
-      <img id="display0" height="256px"/>
-      <img id="display1" height="256px"/>
+      <img id="display0" height="256px" ref={element => this.imageElements.push(element!)}/>
+      <img id="display1" height="256px" ref={element => this.imageElements.push(element!)}/>
       <hr/>
       <img id="result" height="256px" className="center"/>
     </div>
   }
 
   change(index: number) {
-    const element = this.imageElements[index];
+    const element = this.inputElements[index];
     const file = element.files![0];
     const reader = new FileReader();
     reader.readAsBinaryString(file);
     reader.onload = () => {
-      (document.getElementById(`display${index}`) as HTMLImageElement).src=`data:image/png;base64,${btoa(reader.result as string)}`;
+      // (document.getElementById(`display${index}`) as HTMLImageElement).src=`data:image/png;base64,${btoa(reader.result as string)}`;
+      this.imageElements[index].src=`data:image/png;base64,${btoa(reader.result as string)}`;
       this.makeGif();
     };
   }
