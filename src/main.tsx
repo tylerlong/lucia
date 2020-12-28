fa3df85e54b9dab328f794f58a38587e4dd0341b
Tyler Long <tyler4long@gmail.com>
2020-12-27 21:22:48 -0800

    Slider ref

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,12 @@ import gifshot from 'gifshot';
 import { Slider } from 'antd';
 
 class App extends React.Component {
-  sliderRef: React.RefObject<typeof Slider>;
+  slider?: typeof Slider;
   inputElements: HTMLInputElement[];
   imageElements: HTMLImageElement[];
 
   constructor(props: any) {
     super(props);
-    this.sliderRef = React.createRef();
     this.inputElements = [];
     this.imageElements = [];
   }
@@ -22,7 +21,8 @@ class App extends React.Component {
       </p>
       <input type="file" id="upload0" onChange={e => this.change(0)} ref={element => this.inputElements.push(element!)}/>
       <input type="file" id="upload1" onChange={e => this.change(1)} ref={element => this.inputElements.push(element!)}/>
-      <br/><br/>Animation speed: <Slider min={1} max={100} defaultValue={10} onAfterChange={() => this.makeGif()} ref={this.sliderRef}/>
+      <br/><br/>Animation speed: <Slider min={1} max={100} defaultValue={10} 
+        onAfterChange={() => this.makeGif()} ref={(element: typeof Slider) => this.slider = element}/>
       <hr/>
       <img id="display0" height="256px" ref={element => this.imageElements.push(element!)}/>
       <img id="display1" height="256px" ref={element => this.imageElements.push(element!)}/>
@@ -37,14 +37,13 @@ class App extends React.Component {
     const reader = new FileReader();
     reader.readAsBinaryString(file);
     reader.onload = () => {
-      // (document.getElementById(`display${index}`) as HTMLImageElement).src=`data:image/png;base64,${btoa(reader.result as string)}`;
       this.imageElements[index].src=`data:image/png;base64,${btoa(reader.result as string)}`;
       this.makeGif();
     };
   }
 
   makeGif() {
-    const sliderValue = (this.sliderRef.current as any).state.value;
+    const sliderValue = (this.slider! as any).state.value;
     const img1 = document.getElementById('display0') as HTMLImageElement;
     const img2 = document.getElementById('display1') as HTMLImageElement;
     if(img1.src !== '' && img2.src !== '') {
