1294d8723dd856617523974f415d7952dba0d7b0
Tyler Long <tyler4long@gmail.com>
2020-12-27 22:24:00 -0800

    Dynamic size

--- src/main.tsx
+++ src/main.tsx
@@ -2,51 +2,54 @@ import React from 'react';
 import gifshot from 'gifshot';
 import { Slider } from 'antd';
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
   slider?: typeof Slider;
-  inputElements: HTMLInputElement[];
-  imageElements: HTMLImageElement[];
+  inputElement?: HTMLInputElement;
   resultImage?: HTMLImageElement;
 
   constructor(props: any) {
     super(props);
-    this.inputElements = [];
-    this.imageElements = [];
+    this.state = { size: 0 };
   }
 
   render() {
-    const size = 5;
     return <div>
       <h1>Project Lucia</h1>
       <p>
         Lucia is derived from lux, the Latin word for light.
       </p>
-      {new Array(size).fill(1).map((e, i) => <input type="file" onChange={e => this.change(i)} ref={element => this.inputElements.push(element!)} key={i}/>)}
+      <hr/>
+      <div id="images"></div>
+      <br/>Upload new image: <input type="file" onChange={e => this.change()} ref={element => this.inputElement = element!}/>
       <br/><br/>Animation speed: <Slider min={1} max={100} defaultValue={10} 
         onAfterChange={() => this.makeGif()} ref={(element: typeof Slider) => this.slider = element}/>
-      <hr/>
-      {new Array(size).fill(1).map((e, i) => <img height="256px" ref={element => this.imageElements.push(element!)} key={i}/>)}
       <hr/>
       Preview: <br/>
       <img height="256px" className="center" ref={element => this.resultImage = element!}/>
     </div>
   }
 
-  change(index: number) {
-    const element = this.inputElements[index];
+  change() {
+    this.setState({size: this.state.size + 1});
+    const element = this.inputElement!;
     const file = element.files![0];
     const reader = new FileReader();
     reader.readAsBinaryString(file);
     reader.onload = () => {
-      this.imageElements[index].src=`data:image/png;base64,${btoa(reader.result as string)}`;
+      const image = document.createElement('img');
+      image.src = `data:image/png;base64,${btoa(reader.result as string)}`;
+      image.height = 256;
+      image.className="image"
+      document.getElementById('images')!.appendChild(image);
       this.makeGif();
     };
+    element.value='';
   }
 
   makeGif() {
     const sliderValue = (this.slider! as any).state.value;
       gifshot.createGIF({
-        images: this.imageElements.filter(e => e.src !== ''),
+        images: Array.from(document.getElementsByClassName('image')),
         gifHeight: 256,
         frameDuration: 100 / sliderValue,
       }, (obj: any) => {
